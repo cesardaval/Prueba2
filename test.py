1603d881ae40715d69797b87ab1b67d5b6e416e4
Msteroz <cesarhquinterod@gmail.com>
2020-01-13 09:53:03 -0400

    en este commint inclui el soporte para el reporte en su manera mas basica todavia falta implementar una plantilla real para que podamos ejecutar el reporte de forma correcta

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from models import User, Representante ,Preinscripcion, db
+"""from models import User, Representante ,Preinscripcion, db
 
 
 u = User("cesar", "24370551", "cesardaval", "cesarhquinterod@gmail.com"
@@ -13,3 +13,52 @@ db.session.add(u)
 db.session.add(r)
 db.session.add(p)
 db.session.commit()
+
+"""
+from flask import Flask, render_template,url_for
+from flask_weasyprint import HTML, render_pdf
+from models import User, db, Representante, Preinscripcion
+from tables import Tabla
+from config import Configuracion_desarrollo
+app = Flask(__name__)
+app.config.from_object(Configuracion_desarrollo)
+
+
+@app.route('/reporte.pdf')
+def hola():
+	repre = db.session.query(User,Representante).join(User).add_columns(User.username,User.email
+        , Representante.id)
+	tablita = Tabla(repre)
+	prueba = render_template("tabla.html", tabla=tablita)
+
+
+	return render_pdf(HTML(string=prueba))
+
+@app.route('/edita/<id>', methods=["GET", "POST"])
+def edita(id):
+    id = id
+
+    return "tu id es: {}".format(id)
+
+
+
+@app.route('/hello/', defaults={'name': 'World'})
+@app.route('/hello/<name>/')
+def hello_html(name):
+    return render_template('hello.html', name=name)
+
+
+# Alternatively, if the PDF does not have a matching HTML page:
+
+@app.route('/hello_<name>.pdf')
+def hello_pdf(name):
+    # Make a PDF straight from HTML in a string.
+    html = render_template('hello.html', name=name)
+    return render_pdf(HTML(string=html))
+
+
+
+
+if __name__ == '__main__':
+	db.init_app(app)
+	app.run(debug=True,port=7000)
